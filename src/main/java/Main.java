47b0419a1236c87e52dbcdbb9b79a07145ac43ab
shakap12 <shashwatkapoor14@gmail.com>
2025-04-12 14:42:30 +0530

    added pattern priting logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hllo World! I'm learning git");
+
+        System.out.println("Printing pattern!");
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
     }
 }
