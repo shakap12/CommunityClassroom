fd8b11551ec98456c31769fa56fd997ae33807c9
shakap12 <shashwatkapoor14@gmail.com>
2025-04-12 14:44:29 +0530

    added second logic for pattern

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,13 @@ public class Main {
             }
             System.out.println();
         }
+
+        System.out.println("Printing logic 2 for pattern");
+        for(int i=5;i>=0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
 }
